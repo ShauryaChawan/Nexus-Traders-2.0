f22c4089348819775794573e513f95be322401e0
Shaurya <shauryachawan4646@gmail.com>
2023-06-19 21:02:24 +0530

    Added SEO - React Helmet Updated

--- nexus-traders-frontend-2.0/src/App.jsx
+++ nexus-traders-frontend-2.0/src/App.jsx
@@ -1,5 +1,6 @@
 import "./app.scss";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
@@ -45,6 +46,32 @@ function App() {
 
   return (
     <div className="app_container">
+      {/* <Helmet>
+        <meta
+          name="description"
+          content="Nexus Traders Trading School. Turning Wealth into Empire !!"
+        />
+        <meta
+          name="keywords"
+          content="Best Trading School in Thane, \
+          Best Stock Maket Trading School in Thane, \
+          Best Share Maket Trading School in Thane, \
+          Best Trading Classes in Thane, \
+          Best Stock Maket Trading Classes in Thane, \
+          Best Share Maket Trading Classes in Thane, \
+          Best Trading Coaching in Thane, \
+          Best Stock Maket Trading Coaching in Thane, \
+          Best Share Maket Trading Coaching in Thane, \
+          Trading Coaching, \
+          Trading Classes, \
+          Stock Market Trading Coaching, \
+          Share Market Trading Coaching, \
+          Stock Market Trading Classes, \
+          Share Market Trading Classes, \
+          Forex Trading Coaching, \
+          Forex Trading Classes,"
+        />
+      </Helmet> */}
       <RouterProvider router={router} />
     </div>
   );
